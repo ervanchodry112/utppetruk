7d7df656ab9e1275b7e74582cf7cfb821ec63c19
Ervan Chodry <ervanchodry112@gmail.com>
2021-05-18 18:02:38 +0700

    tambah addNewtask

--- todolist.cpp
+++ todolist.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 
 /* PROGRAM TO DO LIST 
 Anggota Kelompok :
 1. Ervan Chodry				2017051001
-2. Aullya Hannan Wulandari	2017051079	*/
+2. Aullya Hannan Wulandari	2017051079*/
 
 struct node{
 	char task[101];
@@ -13,30 +12,62 @@ struct node{
 };
 
 void print(node *point);
-void menu();	
+void menu();
+char *input();
+node *addnewtask(node *start, char *do);
+
 
 int main(){
 	node *job = NULL;
 	int pilih = 1;
-	
-	do{
+	char *word;
+
+	while(true){
+		system("cls");
 		switch(pilih){
 			case 1:
-				cout << menu() << endl;
-				cout << "Masukkan pilihan : ";
 				cin >> pilih;
 				break;
 
+			case 2:
+				cout << "Masukkan tugas yang ingin ditambahkan : ";
+				// word = input
+				cin.getline(word, 100);
+				job = addnewtask(job, word)
+
+			case 4:
+				exit(1);
+
+			default:
+				cout << "Input yang anda masukkan tidak valid" << endl;
+				system("pause");
 		}
-	}while(pilih != 4)
+	}
 
 }
 
 
 
 void print(node *point){
+	node *p;
 	if (point == NULL)
 	{
 		cout << "Belum Ada Tugas yang harus diselesaikan!";
 	}
+
+	p = point;
+
+	while(p != NULL){
+		cout << p->task << endl;
+		p = p->address;
+	}
+
+}
+
+node *addnewtask(node *start, char *newTask){
+	node *temp = new node;
+	temp->task = newTask;
+	temp->address = start;
+	start = temp;
+	return start;
 }
